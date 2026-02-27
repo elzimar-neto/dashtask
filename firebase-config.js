e31a88bd5bff7217b3aeb647065c2664148b7ccf
elzimar-neto <elzimar.neto@gmail.com>
2026-02-26 22:10:33 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, collection, addDoc, query, where, getDocs, onSnapshot, updateDoc, doc, deleteDoc, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { 
+    getFirestore, collection, addDoc, query, where, getDocs, 
+    onSnapshot, updateDoc, doc, deleteDoc, serverTimestamp 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { 
+    getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBEAd17qKJ1ye2qA8ZpNS8OiUYdwwR9XcY",
@@ -16,4 +21,9 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
-export { db, auth, provider, signInWithPopup, signOut, onAuthStateChanged, collection, addDoc, query, where, getDocs, onSnapshot, updateDoc, doc, deleteDoc, serverTimestamp };
+// Exportação completa para o app.js
+export { 
+    db, auth, provider, signInWithPopup, signOut, onAuthStateChanged, 
+    collection, addDoc, query, where, getDocs, onSnapshot, 
+    updateDoc, doc, deleteDoc, serverTimestamp 
+};
